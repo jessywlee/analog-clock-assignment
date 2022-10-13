98b8408db10645c19d49912d0bf6b8b5639f443e
yewon.lee <yewon.lee@seavantage.com>
2022-10-14 00:55:49 +0900

    initial commit with layout

---
+++ src/components/AnalogClock.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="clock-container">
+    <div class="clock">
+      <img class="clock-img" src="@/assets/seoul.jpg" />
+      <ClockHand />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ClockHand from "./ClockHand.vue";
+
+export default defineComponent({
+  name: "AnalogClock",
+  components: { ClockHand },
+  props: {},
+});
+</script>
+<style scoped>
+.clock-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.clock {
+  position: relative;
+  width: 400px;
+  height: 400px;
+  background: url("@/assets/clock.png");
+  border-radius: 50%;
+  border-color: black;
+  box-shadow: -4px -4px 10px rgba(67, 67, 67, 0.5),
+    inset 4px 4px 10px rgba(0, 0, 0, 0.5),
+    inset -4px -4px 10px rgba(67, 67, 67, 0.5), 4px 4px 10px rgba(0, 0, 0, 0.3);
+}
+
+.clock-img {
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  opacity: 0.35;
+}
+</style>
---
+++ src/components/ClockHand.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hand-container"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ClockHand",
+  props: {},
+});
+</script>
+<style scoped>
+.hand-container {
+  position: absolute;
+  width: 50%;
+  right: 50%;
+  bottom: 50%;
+  height: 6px;
+  background-color: black;
+}
+</style>
