afa39d4fd6b4cf06e32703c12e00691230cab08d
yewon.lee <yewon.lee@seavantage.com>
2022-10-15 03:25:20 +0900

    [add] vuex - state, getter, mutation 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,28 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    now: new Date(),
+  },
+  getters: {
+    rotationDegreesHours(state) {
+      const hours = state.now.getHours();
+      return (hours / 12) * 360 + (state.now.getMinutes() / 60) * 30 + 90;
+    },
+    rotationDegreesMinutes(state) {
+      const mins = state.now.getMinutes();
+      return (mins / 60) * 360 + (state.now.getSeconds() / 60) * 6 + 90;
+    },
+    rotationDegreesSeconds(state) {
+      const seconds = state.now.getSeconds();
+      return (seconds / 60) * 360 + 90;
+    },
+  },
+  mutations: {
+    getNewDate(state) {
+      state.now = new Date();
+    },
+  },
   actions: {},
   modules: {},
 });
